9d70af4582d85320aebe30655c67d6db11a22e2b
MetaSource <jsal91@gmail.com>
2023-01-05 03:00:09 -0800

    Scaffold home screen, prepare for NFTCard screen scaffold

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { COLORS,SIZES, SHADOWS, assets} from '../constants';
 
 const NFTCard = () => {
   return (
-    <View>
-      <Text>NFTCard</Text>
+    <View style>
+      
     </View>
   )
 }
