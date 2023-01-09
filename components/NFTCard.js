9ec5d404979d7d69405679d30ef670166d6fc849
MetaSource <jsal91@gmail.com>
2023-01-09 02:32:24 -0800

    Update components, views

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,6 +1,8 @@
 import { View, Image, Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { COLORS,SIZES, SHADOWS, assets} from '../constants';
+import { CircleButton, RectButton } from './Button';
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo';
 
 const NFTCard = ({data}) => {
     const navigation = useNavigation();
@@ -23,8 +25,31 @@ const NFTCard = ({data}) => {
                  borderTopRightRadius:SIZES.font,
              }}
              />
+             <CircleButton imgUrl={assets.heart} right={10} top={10}/>          
         </View>
-        <Text>NFT Card</Text>
+        
+        <SubInfo/>
+
+        <View style={{
+            width: "100%",
+            padding : SIZES.font,
+            }}>
+            <NFTTitle 
+                title={data.name} 
+                subTitle={data.creator} 
+                titleSize={SIZES.large}
+                subTitleSize={SIZES.small}
+            />
+            <View style={{
+                marginTop: SIZES.font,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+                }}>
+                <EthPrice price={data.price}/>
+                <RectButton minWidth={120} fontSize={SIZES.font} handlePress={()=> navigation.navigate("Details", {data})}/>
+            </View>
+        </View>
     </View>
   )
 }
